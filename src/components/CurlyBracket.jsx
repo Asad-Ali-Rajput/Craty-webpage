37e11efbdee4c5171ce94d8d5ebf0d43c2327006
Asad-Ali-Rajput <asadalirajput453@gmail.com>
2023-08-22 14:27:40 +0500

    Carty WebPage

---
+++ src/components/CurlyBracket.jsx
@@ -0,0 +1,15 @@
+// components/DivWithStomach.js
+import React from "react";
+// import styles from "./DivWithStomach.module.css";
+
+const DivWithStomach = () => {
+  return (
+    <div className="custom-border">
+        <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+  <path fill="#FF0066" d="M-90,-85H90Q90,90 185,90V-90Q-90,-90 -90,-85Z" />
+</svg>
+    </div>
+  );
+};
+
+export default DivWithStomach;
