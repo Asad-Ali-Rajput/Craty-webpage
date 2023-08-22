d863d468edd1ed142cba683d65ff65471ab914ba
Asad-Ali-Rajput <asadalirajput453@gmail.com>
2023-08-22 14:28:59 +0500

    Carty WebPage

---
+++ src/components/Info.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+export default function Info () {
+    return (
+        <div className="w-full flex justify-around pt-10 items-center bg-white p-6">
+            <div className="w-1/3 font-extrabold text-[40px]">
+                <h1>Get to know more about unviers</h1>
+            </div>
+            <div className="w-1/3 text-slate-400">
+                <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque totam eaque fugiat nulla. Illo porro eum fugit cum officia, sed iure incidunt, minima harum reiciendis enim ut error minus exercitationem!</h1>
+            </div>
+        </div>
+    )
+}
